4c021de4e2140f330543495307a620522d91b1ed
Dustin Carlino <dabreegster@gmail.com>
2023-12-02 20:05:09 +0800

    Show a map and GJ

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import turfBbox from "@turf/bbox";
   import init, { MapModel } from "backend";
   import { onMount } from "svelte";
+  import { GeoJSON, LineLayer, MapLibre, Popup } from "svelte-maplibre";
   import xmlUrl from "../assets/input.osm?url";
   import Layout from "./Layout.svelte";
   import Loading from "./Loading.svelte";
 
   let model: MapModel | undefined = undefined;
+  let map;
   let loading = false;
 
   onMount(async () => {
@@ -13,8 +16,7 @@
     try {
       loading = true;
       let resp = await fetch(xmlUrl);
-      let buffer = await resp.arrayBuffer();
-      model = new MapModel(new Uint8Array(buffer));
+      loadModel(await resp.arrayBuffer());
     } catch (err) {
       window.alert(`Couldn't open from URL ${xmlUrl}: ${err}`);
     }
@@ -25,12 +27,23 @@
   async function loadFile(e: Event) {
     try {
       loading = true;
-      let buffer = await fileInput.files![0].arrayBuffer();
-      model = new MapModel(new Uint8Array(buffer));
+      loadModel(await fileInput.files![0].arrayBuffer());
     } catch (err) {
       window.alert(`Couldn't open this file: ${err}`);
     }
     loading = false;
+  }
+
+  function loadModel(buffer: Buffer) {
+    model = new MapModel(new Uint8Array(buffer));
+  }
+
+  function zoomToFit() {
+    if (map && model) {
+      // TODO wasteful
+      let bbox = turfBbox(JSON.parse(model.render()));
+      map.fitBounds(bbox, { animate: false });
+    }
   }
 </script>
 
@@ -39,9 +52,32 @@
     <label>
       <input bind:this={fileInput} on:change={loadFile} type="file" />
     </label>
+    <div><button on:click={zoomToFit}>Zoom to fit</button></div>
   </div>
   <div slot="main" style="position:relative; width: 100%; height: 100vh;">
-    <p>Canvas goes here</p>
+    <MapLibre
+      style="https://api.maptiler.com/maps/dataviz/style.json?key=MZEJTanw3WpxRvt7qDfo"
+      standardControls
+      hash
+      bind:map
+    >
+      {#if model}
+        <GeoJSON data={JSON.parse(model.render())}>
+          <LineLayer
+            paint={{
+              "line-width": 5,
+              "line-color": "red",
+            }}
+            on:click={(e) =>
+              window.open(e.detail.features[0].properties.way, "_blank")}
+          >
+            <Popup openOn="hover" let:data
+              >{@html JSON.stringify(data.properties, null, "<br />")}</Popup
+            >
+          </LineLayer>
+        </GeoJSON>
+      {/if}
+    </MapLibre>
   </div>
 </Layout>
 <Loading {loading} />
