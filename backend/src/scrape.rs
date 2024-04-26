58d854a3533ef0356ac78e03b3bb6d5a94fa898e
Dustin Carlino <dabreegster@gmail.com>
2024-04-26 17:38:58 +0100

    Refactor graph scaping code. https://github.com/a-b-street/utils/issues/2

--- backend/src/scrape.rs
+++ backend/src/scrape.rs
@@ -1,65 +1,44 @@
-use std::collections::HashMap;
-
 use anyhow::Result;
-use geo::{ConvexHull, Coord, Geometry, GeometryCollection, LineString, Point};
-use osm_reader::{Element, NodeID, WayID};
-use utils::{Mercator, Tags};
+use utils::Tags;
 
 use crate::{Intersection, IntersectionID, MapModel, Road, RoadID, RoadKind};
-
-struct Way {
-    id: WayID,
-    node_ids: Vec<NodeID>,
-    tags: Tags,
-}
 
 pub fn scrape_osm(
     input_bytes: &[u8],
     import_streets_without_sidewalk_tagging: bool,
 ) -> Result<MapModel> {
-    let mut node_mapping = HashMap::new();
-    let mut highways = Vec::new();
-    osm_reader::parse(input_bytes, |elem| match elem {
-        Element::Node { id, lon, lat, .. } => {
-            let pt = Coord { x: lon, y: lat };
-            node_mapping.insert(id, pt);
-        }
-        Element::Way { id, node_ids, tags } => {
-            let tags: Tags = tags.into();
-            if classify(&tags, import_streets_without_sidewalk_tagging).is_some() {
-                highways.push(Way { id, node_ids, tags });
-            }
-        }
-        Element::Relation { .. } => {}
+    let graph = utils::osm2graph::Graph::new(input_bytes, |tags| {
+        classify(tags, import_streets_without_sidewalk_tagging).is_some()
     })?;
 
-    let (mut roads, mut intersections) = split_edges(
-        &node_mapping,
-        highways,
-        import_streets_without_sidewalk_tagging,
-    );
+    // Copy all the fields
+    let intersections = graph
+        .intersections
+        .into_iter()
+        .map(|i| Intersection {
+            id: IntersectionID(i.id.0),
+            point: i.point,
+            node: i.osm_node,
+            roads: i.edges.into_iter().map(|e| RoadID(e.0)).collect(),
+        })
+        .collect();
 
-    // TODO expensive
-    let mut collection: GeometryCollection = roads
-        .iter()
-        .map(|r| Geometry::LineString(r.linestring.clone()))
-        .chain(
-            intersections
-                .iter()
-                .map(|i| Geometry::Point(i.point.clone())),
-        )
-        .collect::<Vec<_>>()
-        .into();
-    let mercator = Mercator::from(collection.clone()).unwrap();
-    for r in &mut roads {
-        mercator.to_mercator_in_place(&mut r.linestring);
-    }
-    for i in &mut intersections {
-        mercator.to_mercator_in_place(&mut i.point);
-    }
-
-    mercator.to_mercator_in_place(&mut collection);
-    let boundary_polygon = collection.convex_hull();
+    // Add in a bit
+    let roads = graph
+        .edges
+        .into_iter()
+        .map(|e| Road {
+            id: RoadID(e.id.0),
+            src_i: IntersectionID(e.src.0),
+            dst_i: IntersectionID(e.dst.0),
+            way: e.osm_way,
+            node1: e.osm_node1,
+            node2: e.osm_node2,
+            linestring: e.linestring,
+            kind: classify(&e.osm_tags, import_streets_without_sidewalk_tagging).unwrap(),
+            tags: e.osm_tags,
+        })
+        .collect();
 
     let (closest_intersection, node_map, ch) = crate::route::build_router(&intersections, &roads);
     let path_calc = fast_paths::create_calculator(&ch);
@@ -67,86 +46,13 @@ pub fn scrape_osm(
     Ok(MapModel {
         roads,
         intersections,
-        mercator,
+        mercator: graph.mercator,
         closest_intersection,
         node_map,
         ch,
         path_calc,
-        boundary_polygon,
+        boundary_polygon: graph.boundary_polygon,
     })
-}
-
-fn split_edges(
-    node_mapping: &HashMap<NodeID, Coord>,
-    ways: Vec<Way>,
-    import_streets_without_sidewalk_tagging: bool,
-) -> (Vec<Road>, Vec<Intersection>) {
-    // Count how many ways reference each node
-    let mut node_counter: HashMap<NodeID, usize> = HashMap::new();
-    for way in &ways {
-        for node in &way.node_ids {
-            *node_counter.entry(*node).or_insert(0) += 1;
-        }
-    }
-
-    // Split each way into edges
-    let mut node_to_intersection: HashMap<NodeID, IntersectionID> = HashMap::new();
-    let mut intersections = Vec::new();
-    let mut roads = Vec::new();
-    for way in ways {
-        let mut node1 = way.node_ids[0];
-        let mut pts = Vec::new();
-
-        let num_nodes = way.node_ids.len();
-        for (idx, node) in way.node_ids.into_iter().enumerate() {
-            pts.push(node_mapping[&node]);
-            // Edges start/end at intersections between two ways. The endpoints of the way also
-            // count as intersections.
-            let is_endpoint =
-                idx == 0 || idx == num_nodes - 1 || *node_counter.get(&node).unwrap() > 1;
-            if is_endpoint && pts.len() > 1 {
-                let road_id = RoadID(roads.len());
-
-                let mut i_ids = Vec::new();
-                for (n, point) in [(node1, pts[0]), (node, *pts.last().unwrap())] {
-                    let intersection = if let Some(i) = node_to_intersection.get(&n) {
-                        &mut intersections[i.0]
-                    } else {
-                        let i = IntersectionID(intersections.len());
-                        intersections.push(Intersection {
-                            id: i,
-                            node: n,
-                            point: Point(point),
-                            roads: Vec::new(),
-                        });
-                        node_to_intersection.insert(n, i);
-                        &mut intersections[i.0]
-                    };
-
-                    intersection.roads.push(road_id);
-                    i_ids.push(intersection.id);
-                }
-
-                roads.push(Road {
-                    id: road_id,
-                    src_i: i_ids[0],
-                    dst_i: i_ids[1],
-                    way: way.id,
-                    node1,
-                    node2: node,
-                    linestring: LineString::new(std::mem::take(&mut pts)),
-                    tags: way.tags.clone(),
-                    kind: classify(&way.tags, import_streets_without_sidewalk_tagging).unwrap(),
-                });
-
-                // Start the next edge
-                node1 = node;
-                pts.push(node_mapping[&node]);
-            }
-        }
-    }
-
-    (roads, intersections)
 }
 
 /// This function classifies an OSM way as a RoadKind. If it returns `None`, then the way is
