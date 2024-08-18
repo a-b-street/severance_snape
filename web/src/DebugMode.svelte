428a7bba791688f8e932c3f0ced50d74f1a7ebd3
Dustin Carlino <dabreegster@gmail.com>
2024-08-18 13:18:12 +0100

    Tool to roughly update OSM data somewhere

--- web/src/DebugMode.svelte
+++ web/src/DebugMode.svelte
@@ -2,8 +2,13 @@
   import { GeoJSON, hoverStateFilter, LineLayer } from "svelte-maplibre";
   import { kindToColor } from "./colors";
   import { SplitComponent } from "svelte-utils/top_bar_layout";
-  import { PropertiesTable, notNull } from "svelte-utils";
+  import {
+    PropertiesTable,
+    notNull,
+    downloadGeneratedFile,
+  } from "svelte-utils";
   import { Popup, constructMatchExpression } from "svelte-utils/map";
+  import { overpassQueryForPolygon } from "svelte-utils/overpass";
   import { model } from "./stores";
   import NavBar from "./NavBar.svelte";
 
@@ -11,6 +16,19 @@
 
   export let showSeverances: boolean;
   export let opacity: number;
+
+  async function updateOsm() {
+    // TODO The original clipping boundary isn't preserved. This will grow the
+    // area size every use.
+    let gj = JSON.parse($model!.getInvertedBoundary());
+    // Un-invert the boundary
+    let boundary = gj.geometry.coordinates.pop();
+    gj.geometry.coordinates = [boundary];
+
+    let resp = await fetch(overpassQueryForPolygon(gj));
+    let xml = await resp.text();
+    downloadGeneratedFile("updated_osm.xml", xml);
+  }
 </script>
 
 <SplitComponent>
@@ -18,6 +36,8 @@
   <div slot="sidebar">
     <h2>Debug mode</h2>
     <p>Hover to see a segment's properties, and click to open OSM</p>
+
+    <button on:click={updateOsm}>Update OSM data</button>
   </div>
   <div slot="map">
     <GeoJSON data={JSON.parse(notNull($model).render())} generateId>
