e9653975dae8f74c694baef43632ed1864608664
Dustin Carlino <dabreegster@gmail.com>
2025-06-20 16:50:59 +0100

    Speed up the collapse_degree_2 algorithm by making just one pass. From about 9s in liverpool to sub-second.

--- backend/src/join_lines.rs
+++ backend/src/join_lines.rs
@@ -21,6 +21,15 @@ impl KeyedLineString {
     fn last_pt(&self) -> HashedPoint {
         HashedPoint::new(*self.linestring.0.last().unwrap())
     }
+
+    // TODO Assumes not a loop
+    fn other_endpt(&self, pt: HashedPoint) -> HashedPoint {
+        if self.first_pt() == pt {
+            self.last_pt()
+        } else {
+            self.first_pt()
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -34,37 +43,57 @@ impl HashedPoint {
 }
 
 /// Find all linestrings that meet at one end and join them
+// TODO Test with a loop consisting of two inputs
 pub fn collapse_degree_2(input_lines: Vec<KeyedLineString>) -> Vec<KeyedLineString> {
     // Assign each input an ID that doesn't change
     let mut lines: HashMap<usize, KeyedLineString> = input_lines.into_iter().enumerate().collect();
     let mut id_counter = lines.len();
 
-    // TODO Do this in one pass, either with something like union-find, just a search starting from
-    // any degree 2, or by updating point_to_line indices
-    loop {
-        // How many lines connect to each point?
-        let mut point_to_line: HashMap<HashedPoint, Vec<usize>> = HashMap::new();
-        for (id, line) in &lines {
-            point_to_line
-                .entry(line.first_pt())
-                .or_insert_with(Vec::new)
-                .push(*id);
-            point_to_line
-                .entry(line.last_pt())
-                .or_insert_with(Vec::new)
-                .push(*id);
-        }
+    // How many lines connect to each point?
+    let mut point_to_line: HashMap<HashedPoint, Vec<usize>> = HashMap::new();
+    for (id, line) in &lines {
+        point_to_line
+            .entry(line.first_pt())
+            .or_insert_with(Vec::new)
+            .push(*id);
+        point_to_line
+            .entry(line.last_pt())
+            .or_insert_with(Vec::new)
+            .push(*id);
+    }
 
-        // Find any degree 2 case
-        let Some(pair) = point_to_line.into_values().find(|list| list.len() == 2) else {
-            break;
-        };
+    // Find all degree 2 cases
+    let degree_two: Vec<HashedPoint> = point_to_line
+        .iter()
+        .filter(|(_, list)| list.len() == 2)
+        .map(|(pt, _)| *pt)
+        .collect();
+
+    // Fix them
+    for pt in degree_two {
+        let pair = point_to_line.remove(&pt).unwrap();
         let (idx1, idx2) = (pair[0], pair[1]);
 
         let line1 = lines.remove(&idx1).unwrap();
         let line2 = lines.remove(&idx2).unwrap();
+        let other_endpt1 = line1.other_endpt(pt);
+        let other_endpt2 = line2.other_endpt(pt);
+
         let joined = join_lines(line1, line2);
         lines.insert(id_counter, joined);
+
+        // Fix up point_to_line
+        replace(
+            point_to_line.get_mut(&other_endpt1).unwrap(),
+            idx1,
+            id_counter,
+        );
+        replace(
+            point_to_line.get_mut(&other_endpt2).unwrap(),
+            idx2,
+            id_counter,
+        );
+
         id_counter += 1;
     }
 
@@ -113,5 +142,13 @@ fn join_lines(mut line1: KeyedLineString, mut line2: KeyedLineString) -> KeyedLi
 fn flip_direction(ids: &mut Vec<(RoadID, bool)>) {
     for pair in ids {
         pair.1 = !pair.1;
+    }
+}
+
+fn replace(indices: &mut Vec<usize>, old: usize, new: usize) {
+    for x in indices {
+        if *x == old {
+            *x = new;
+        }
     }
 }
